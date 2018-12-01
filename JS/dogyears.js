f6298dbac7e01946e1eca7ea775861ced129b8dd
Scott Gregg <scotttgregg@gmail.com>
2018-12-01 10:50:27 -0800

    dogYears lab

---
+++ JS/dogyears.js
@@ -0,0 +1,10 @@
+let myAge = 30
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+myAgeInDogYears = earlyYears + laterYears
+let myName = 'Scott'
+myName = myName.toLowerCase()
+
+console.log(`My name is ${myName}.  I am ${myAge} years old in human years, which is ${myAgeInDogYears} years old in dog years.`)
